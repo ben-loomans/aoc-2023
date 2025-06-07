597af260c1465b9d2c058b1399f6a2c833b775e5
Ben Loomans <benjaminloomans@gmail.com>
2025-06-07 21:50:20 +1000

    day 2 parts 1 and 2

--- src/solutions/day_2.rs
+++ src/solutions/day_2.rs
@@ -1,9 +1,160 @@
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::combinator::{all_consuming, map};
+use nom::multi::separated_list1;
+use nom::sequence::terminated;
+use nom::Parser;
+use nom::{sequence::delimited, IResult};
+use nom::character::complete::usize;
+
 pub fn part_one(input: &str) -> Result<String, String> {
-    Err("Unimplemented".to_string())
+    process_input(input).map(|answer| answer.to_string())
+}
+
+struct Game {
+    id: usize,
+    draws: Vec<Draw>
+}
+
+#[derive(Default)]
+struct Draw {
+    red: usize,
+    green: usize,
+    blue: usize,
+}
+
+enum Colour {
+    Red(usize),
+    Green(usize),
+    Blue(usize),
+}
+
+const BAG: Draw = Draw {
+    red: 12,
+    green: 13,
+    blue: 14,
+};
+
+impl Game {
+    fn min_draw(&self) -> Draw {
+        self.draws.iter()
+            .fold(Draw::default(), |acc, draw| {
+                Draw {
+                    red: acc.red.max(draw.red),
+                    green: acc.green.max(draw.green),
+                    blue: acc.blue.max(draw.blue),
+                }
+            })
+    }
+}
+
+impl Draw {
+    fn power(&self) -> usize {
+        self.red * self.green * self.blue
+    }
+}
+
+fn process_input(input: &str) -> Result<usize, String> {
+    input.lines().try_fold(0, |acc, line| {
+        let game = parse_game(line)?;
+        if is_game_possible(&game, &BAG) {
+            Ok(acc + game.id)
+        } else {
+            Ok(acc)
+        }
+    })
+}
+
+fn parse_game(input: &str) -> Result<Game, String> {
+    let (_, (id, draws)) = all_consuming((
+        delimited(
+            tag("Game "), 
+            usize, 
+            tag(": ")
+        ),
+        separated_list1(
+            tag("; "),
+            parse_draw
+        )
+    )).parse(input)
+        .map_err(|_| "issue parsing game".to_string())?;
+
+    Ok(Game {
+        id,
+        draws,
+    })
+}
+
+fn parse_draw(input: &str) -> IResult<&str, Draw> {
+    // e.g. input = "8 green, 6 blue, 20 red"
+    let mut draw = Draw::default();
+
+    let (rest, colours) = separated_list1(
+        tag(", "), 
+        parse_colour
+    ).parse(input)?;
+    
+    for colour in colours {
+        match colour {
+            Colour::Red(num) => draw.red += num,
+            Colour::Green(num) => draw.green += num,
+            Colour::Blue(num) => draw.blue += num,
+        }
+    }
+
+    return Ok((rest, draw))
+}
+
+fn parse_colour(input: &str) -> IResult<&str, Colour> {
+    alt((
+        map(
+            terminated(
+                usize, 
+                tag(" red"),
+            ),
+            |num| Colour::Red(num)
+        ),
+        map(
+            terminated(
+                usize, 
+                tag(" green"),
+            ),
+            |num| Colour::Green(num)
+        ),
+        map(
+            terminated(
+                usize, 
+                tag(" blue"),
+            ),
+            |num| Colour::Blue(num)
+        )
+    )).parse(input)
+}
+
+fn is_game_possible(game: &Game, bag: &Draw) -> bool {
+    for draw in &game.draws {
+        if  draw.red > bag.red || 
+            draw.green > bag.green ||
+            draw.blue > bag.blue 
+        {
+            return false;
+        }
+    }
+
+    return true;
 }
 
 pub fn part_two(input: &str) -> Result<String, String> {
-    Err("Unimplemented".to_string())
+    parse_two(input).map(|answer| answer.to_string())
+}
+
+fn parse_two(input: &str) -> Result<usize, String> {
+    input.lines().try_fold(0, |acc, line| {
+        let game = parse_game(line)?;
+        let power = game.min_draw().power();
+
+        Ok(acc + power)
+    })
 }
 
 const _EXAMPLE: &str = "\
