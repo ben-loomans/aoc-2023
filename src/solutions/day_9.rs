be78d6c456fc47f01287ddefa31e56c9fae64498
Ben Loomans <benjaminloomans@gmail.com>
2025-06-30 16:53:27 +1000

    set up day 9

---
+++ src/solutions/day_9.rs
@@ -0,0 +1,24 @@
+pub fn part_one(input: &str) -> Result<String, String> {
+    Err("Unimplemented".to_string())
+}
+
+pub fn part_two(input: &str) -> Result<String, String> {
+    Err("Unimplemented".to_string())
+}
+
+const _EXAMPLE: &str = "\
+0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
+
+const _ANSWER: &str = "114";
+
+#[test]
+fn test_part_one() {
+    // assert_eq!(_ANSWER, &part_one(_EXAMPLE).unwrap());
+}
+
+#[test]
+fn test_part_two() {
+    // assert_eq!(_ANSWER, &part_two(_EXAMPLE).unwrap());
+}
